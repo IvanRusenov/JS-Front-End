72790acb1eece0b5e4f487079ee275815b8de2c0
IvanRusenov <i_rusenov@abv.bg>
2024-03-27 08:11:46 +0200

    Create 3. Even and Odd Subtraction.js

---
+++ 04. JS Arrays and Strings-Lab/3. Even and Odd Subtraction.js
@@ -0,0 +1,17 @@
+function solve(arr) {
+
+    let evenSum = 0;
+    let oddSum = 0;
+
+    for (const el of arr) {
+        if (el % 2 == 0) {
+            evenSum += el;
+        } else {
+            oddSum += el;
+        }
+    }
+
+    console.log(evenSum - oddSum);
+}
+
+solve([1, 2, 3, 4, 5, 6]);
