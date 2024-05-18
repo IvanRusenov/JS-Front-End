e1df93c45bd0e408d0872689ea23507730da605c
IvanRusenov <i_rusenov@abv.bg>
2024-05-18 09:50:38 +0200

    Create SumNumbers.js

---
+++ 13. DOM Introduction - Lab/01. Sum-Numbers/SumNumbers.js
@@ -0,0 +1,10 @@
+function calc() {
+
+    let num1 = document.getElementById("num1");
+    let num2 = document.getElementById("num2");
+    let sum = document.getElementById("sum");
+
+    sum.value = Number(num1.value) + Number(num2.value);
+    
+    // TODO: sum = num1 + num2
+}
